c9b9f0a7229a8dc83e5661b4d6582a241d54188b
Marek <marek.majde@gmail.com>
2014-08-30 01:08:51 +0200

    Dodanie map w mojej, początkowej wersji.

--- app/src/main/java/software/pama/run/MainActivity.java
+++ app/src/main/java/software/pama/run/MainActivity.java
@@ -1,19 +1,28 @@
 package software.pama.run;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MyActivity extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
+        final Button button = (Button) findViewById(R.id.btn_run);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+            }
+        });
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
