de8b9e3607f17ee9d233fa92a0a2e8d99a631bb5
Marek <marek.majde@gmail.com>
2014-09-16 01:20:09 +0200

    Moje

--- app/src/main/java/software/pama/run/MainActivity.java
+++ app/src/main/java/software/pama/run/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 
 public class MainActivity extends Activity {
     //moje zmiany w programie
+    // a tu moje
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
